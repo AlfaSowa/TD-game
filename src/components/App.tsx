1863763876153e4aaafb48cfc897810a15a7d327
AlfaSowa <grimmino@gmail.com>
2024-10-28 13:03:28 +0500

    rework auth logic

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,9 +6,7 @@ import { Layout } from './entities'
 
 const tg = (window as any).Telegram.WebApp
 
-const initDevelopData =
-  'user=%7B%22id%22%3A209031846%2C%22first_name%22%3A%22Alex%22%2C%22last_name%22%3A%22SOWA%22%2C%22username%22%3A%22alfa_sowa%22%2C%22language_code%22%3A%22en%22%2C%22is_premium%22%3Atrue%2C%22allows_write_to_pm%22%3Atrue%7D&chat_instance=5073044584243213687&chat_type=sender&auth_date=1730100547&hash=b0feece23e1ce31a19fa7a71d8b32f2a2bfcf7111e0a322ee6fa904de8cc1af0'
-const data = process.env.NODE_ENV === 'development' ? initDevelopData : tg.initData
+const data = process.env.NODE_ENV === 'development' ? process.env.REACT_APP_USER_DATA : tg.initData
 
 export const App = () => {
   setCookie('innerData', data, 365)
