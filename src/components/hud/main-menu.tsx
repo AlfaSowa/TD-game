cf0fa24994a0787a63ba7f153c378bf17224dfb6
AlfaSowa <grimmino@gmail.com>
2024-10-29 13:45:01 +0500

    update welcome text

--- src/components/hud/main-menu.tsx
+++ src/components/hud/main-menu.tsx
@@ -16,13 +16,17 @@ export const MainMenu = ({ game, setGame }: MainMenuProps) => {
   return (
     <div className="h-dvh flex flex-col p-4 bg-[var(--bg)]">
       <div className="border-dotted flex-1 flex flex-col gap-6 border-4 border-[var(--primary)] p-4 [border-image:linear-gradient(var(--primary),transparent_20%,transparent_80%,var(--primary)_100%)_1]">
-        {/* <img className="max-h-80 object-scale-down" src={Logo} alt="" /> */}
-
         <div className="flex-1 flex items-center justify-center">
-          {/* <h1 className="text-center text-[var(--primary)]">CHAPTER 1</h1> */}
-          <h3 className="text-[var(--primary)] text-center">
-            В ожиданийи чего-то грандиозного попробуй вырастить больше всех кукурузы
-          </h3>
+          <div>
+            <h3 className="text-[var(--primary)] text-center">
+              воу воу воу МЫ НАЧИНАЕМ!!! Заходи в игру крась квадратики и собирай с покрашеных квадратов coins каждые 3
+              часа! Если не [сообщение удалено] конечно
+            </h3>
+
+            <h4 className="text-[var(--primary)] text-center">
+              А и запросы обрабатываются медленно так как мы нищие(пока что) и сервисы используем бесплатные
+            </h4>
+          </div>
         </div>
 
         <div className="flex flex-col gap-5 items-center">
