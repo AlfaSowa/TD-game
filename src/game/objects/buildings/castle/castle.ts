9a01eb8dfc3c20ba5be031c4e3832a738775c985
AlfaSowa <grimmino@gmail.com>
2024-12-05 18:13:18 +0500

    added some sprites

--- src/game/objects/buildings/castle/castle.ts
+++ src/game/objects/buildings/castle/castle.ts
@@ -13,5 +13,6 @@ export class Castle extends BaseBuild {
       this.position.set(this.parent.width / 2 - this.width / 2, this.parent.height / 2 - this.height / 2)
     }
   }
+
   update() {}
 }
--- src/game/objects/map-objects/city/city.ts
+++ src/game/objects/map-objects/city/city.ts
@@ -22,6 +22,8 @@ export class City extends BaseMapObject {
 
     this.addChild(sprite)
 
+    console.log(this)
+
     if (this.parent) {
       this.position.set(this.parent.width / 2 - this.width / 2, this.parent.height / 2 - this.height / 2)
     }
