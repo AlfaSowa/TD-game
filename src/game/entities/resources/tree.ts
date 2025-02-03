e29621b4c2d5630e2fe432952580d0303447804d
AlfaSowa <grimmino@gmail.com>
2025-02-03 12:55:16 +0500

    feat: update assets

--- src/game/entities/resources/tree.ts
+++ src/game/entities/resources/tree.ts
@@ -4,8 +4,15 @@ import { Resource } from './resource'
 export class ResourceTree extends Resource {
   async init() {
     const texture = await Assets.loadBundle(['default'])
-    const sprite = new Sprite(texture.default['Tree1.png'])
 
-    this.addChild(sprite)
+    const sprite = new Sprite(texture.default['Tree1.png'])
+    const sprite6 = new Sprite(texture.default['Tree1Shadow.png'])
+
+    sprite.anchor.set(0.5)
+    sprite6.anchor.set(0.5)
+
+    sprite6.y = sprite.height - sprite6.width / 2
+
+    this.addChild(sprite6, sprite)
   }
 }
